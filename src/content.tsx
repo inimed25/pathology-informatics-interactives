3fca19791170cbfbbff185e81632c3875b427f70
inimed25 <ini.med25@gmail.com>
2026-09-16 17:13:33 -0500

    Remove obsolete lesson content loaders

--- src/content.tsx
+++ src/content.tsx
@@ -6,8 +6,7 @@ const introductions = import.meta.glob<MdxModule>("../content/lessons/*/introduc
 const debriefs = import.meta.glob<MdxModule>("../content/lessons/*/debrief.mdx", { eager: true });
 const facultyGuides = import.meta.glob<MdxModule>("../content/lessons/*/faculty.mdx", { eager: true });
 const practicums = import.meta.glob<MdxModule>("../content/lessons/*/practicum.mdx", { eager: true });
-const concepts = import.meta.glob<MdxModule>("../content/lessons/*/concepts.mdx", { eager: true });
-const downtimeNotes = import.meta.glob<MdxModule>("../content/lessons/*/downtime.mdx", { eager: true });
+
 
 const componentFor = (modules: Record<string, MdxModule>, slug: string) => {
   const entry = Object.entries(modules).find(([path]) => path.includes(`/lessons/${slug}/`));
@@ -19,6 +18,4 @@ export const lessonContent = (slug: string) => ({
   Debrief: componentFor(debriefs, slug),
   Faculty: componentFor(facultyGuides, slug),
   Practicum: componentFor(practicums, slug),
-  Concepts: componentFor(concepts, slug),
-  Downtime: componentFor(downtimeNotes, slug),
-});
+ });
